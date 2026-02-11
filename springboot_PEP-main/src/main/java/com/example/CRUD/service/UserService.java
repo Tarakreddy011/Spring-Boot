88994b98f14edbe0813750a311f55fd97e156e2a
Tarakreddy011 <151497094+Tarakreddy011@users.noreply.github.com>
2026-02-11 10:55:54 +0530

    enttity to dto

--- springboot_PEP-main/src/main/java/com/example/CRUD/service/UserService.java
+++ springboot_PEP-main/src/main/java/com/example/CRUD/service/UserService.java
@@ -1,11 +1,16 @@
 package com.example.CRUD.service;
 
 
+import com.example.CRUD.dto.UserDto;
+import com.example.CRUD.dto.UserResponseDto;
+import com.example.CRUD.entity.UserInfo;
+import com.example.CRUD.repository.UserInfoRepository;
 import com.example.CRUD.repository.UserRepository;
 import com.example.CRUD.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,17 +18,67 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    private UserInfoRepository userInfoRepository;
 
+    private User toEntity(UserDto userDto){
+        User user = new User();
+        UserInfo userInfo = new UserInfo();
+        user.setUsername(userDto.getUsername());
+        user.setPassword(userDto.getPassword());
+        user.setEmail(userDto.getEmail());
 
-    public User createUser(User user) {
-        userRepository.save(user);
+        userInfo.setName(userDto.getName());
+        userInfo.setPhone(userDto.getPhone());
+        userInfo.setProfilePic(userDto.getProfilePic());
+        userInfo.setUser(user);
+        user.setUserInfo(userInfo);
+
         return user;
     }
 
-    public List<User> getUsers() {
+    private UserDto toDto(User user){
+        UserDto userDto = new UserDto();
+        UserInfo userInfo = user.getUserInfo();
+        userDto.setUsername(user.getUsername());
+        userDto.setPassword(user.getPassword());
+        userDto.setEmail(user.getEmail());
+        userDto.setPhone(userInfo.getPhone());
+        userDto.setProfilePic(userInfo.getProfilePic());
+        userDto.setName(userInfo.getName());
+
+        return userDto;
+
+    }
+
+    private UserResponseDto toResponseDto(User user){
+        UserResponseDto userDto = new UserResponseDto();
+        UserInfo userInfo = user.getUserInfo();
+        userDto.setUsername(user.getUsername());
+        userDto.setEmail(user.getEmail());
+        userDto.setName(userInfo.getName());
+
+        return userDto;
+    }
+    public User createUser(UserDto userDto) {
+
+
+        User user   = toEntity(userDto) ;
+        userRepository.save(user);
+        userInfoRepository.save(user.getUserInfo());
+
+        return user;
+
+    }
+
+    public List<UserResponseDto> getUsers() {
 
         List<User> users  = userRepository.findAll();
-        return users;
+        List<UserResponseDto> response = new ArrayList<>();
+        for (User user : users) {
+            response.add(toResponseDto(user));
+        }
+        return response;
     }
 
     public User getuser(long id){
